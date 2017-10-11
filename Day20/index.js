68b0236e5030e8ed52c29dc9dfabcf575fe5e6af
Rizky Ramadhan <ramagiox@gmail.com>
2017-10-11 14:17:46 +0700

    ada perubahan
    let surattugasRoute = require('./surattugas/surattugasRoute.js');
    app.use('/api',surattugasRoute);
    
    let tipepegawaiRoute = require('./tipepegawai/tipepegawaiRoute.js');
    app.use('/api',tipepegawaiRoute);
    
    let pegawaiRoute = require('./pegawai/pegawaiRoute.js');
    app.use('/api',pegawaiRoute);
    
    let surattugasrinciRoute = require('./surattugasrinci/surattugasrinciRoute.js');
    app.use('/api',surattugasrinciRoute);

--- Day20/index.js
+++ Day20/index.js
@@ -16,6 +16,18 @@ app.use('/api', gedungRinciRoute);
 let aplikasiDetailRoute = require('./aplikasiDetail/aplikasiDetailRoute.js');
 app.use('/api', aplikasiDetailRoute);
 
+let surattugasRoute = require('./surattugas/surattugasRoute.js');
+app.use('/api',surattugasRoute);
+
+let tipepegawaiRoute = require('./tipepegawai/tipepegawaiRoute.js');
+app.use('/api',tipepegawaiRoute);
+
+let pegawaiRoute = require('./pegawai/pegawaiRoute.js');
+app.use('/api',pegawaiRoute);
+
+let surattugasrinciRoute = require('./surattugasrinci/surattugasrinciRoute.js');
+app.use('/api',surattugasrinciRoute);
+
 //koneksi
 mongoose.connect('mongodb://localhost:27017/DBPelatihan');
 
